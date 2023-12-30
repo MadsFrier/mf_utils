34058cb55596ac80850f297166cf9120ea674072
Mads Frier <madsfrier@gmail.com>
2023-12-30 16:45:42 +0100

    added rob functions for path tracking

--- mfutils/utils.py
+++ mfutils/utils.py
@@ -2,9 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Utility functions
+def rot2d(angle):
+    return np.array([[np.cos(angle), -np.sin(angle)],
+                     [np.sin(angle), np.cos(angle)]])
 
 # Plotting functions
-def _plot_lidar_coords(lidar_coords_x, lidar_coords_y, marker='o', color='r'):
+def plot_lidar_coords(lidar_coords_x, lidar_coords_y, marker='o', color='r'):
     style = marker + color
     return(plt.plot(lidar_coords_x, lidar_coords_y, style))
 
@@ -53,10 +56,9 @@ def plot_arrow(x, y, yaw, arrow_length=1.0,
 
 
 # Robotics functions
-def _lidar_to_coords(robot_pose, lidar_data, lidar_fov, lidar_res):
+def lidar_to_coords(robot_pose, lidar_data, lidar_fov, lidar_res):
     '''
     Convert lidar data to coordinates
-    
     '''
     
     lidar_x_list = []
@@ -71,3 +73,58 @@ def _lidar_to_coords(robot_pose, lidar_data, lidar_fov, lidar_res):
         lidar_y_list.append(lidar_y)
         
     return lidar_x_list, lidar_y_list
+
+def heading_error(pos_1, pos_2, rot_matrix):
+    '''
+    find heading error for pure pursuit controller
+    '''
+    robot_heading = np.dot(rot_matrix, np.array([1, 0]))
+    line = (np.array(pos_2) - np.array(pos_1))
+    angle = np.arccos(np.dot(robot_heading, line)
+                      / np.linalg.norm(line)
+                      / np.linalg.norm(robot_heading))
+    cross = np.cross(line, robot_heading)
+    if cross > 0.0:
+        angle = -angle
+
+    return angle
+
+def track_error(pos_1, pos_2, robot_pos):
+    '''
+    find track error for pure pursuit controller
+    '''
+    A = np.array(pos_1)
+    B = np.array(pos_2)
+    P = np.array([robot_pos[0], robot_pos[1], robot_pos[2]])
+    n = np.cross([0, 0, 1], (B - A))
+    distance = np.dot((P - A), n) / np.linalg.norm(n)
+    return distance
+
+def pure_pursuit(track_error, angle_error, track_param, angle_param):
+    '''
+    Simple pure pursuit controller
+    '''
+    
+    ang_vel = (track_param * -track_error) + (angle_param * angle_error)
+
+    return ang_vel
+
+def get_ddr_wheel_velocities(linear_vel, angular_vel, wheel_radius, max_wheel_vel, wheel_base_length):
+    
+    """Set linear and angular velocities of the robot.
+
+    Arguments:
+    linear_velocity  -- Forward velocity in m/s.
+    angular_velocity -- Rotational velocity rad/s. Positive direction is
+                        counter-clockwise.
+    """
+    
+    diff_vel = angular_vel * wheel_base_length / wheel_radius
+    right_vel = (linear_vel + diff_vel) / wheel_radius
+    left_vel = (linear_vel - diff_vel) / wheel_radius
+    fastest_wheel = abs(max(right_vel, left_vel))
+    if fastest_wheel > max_wheel_vel:
+        left_vel = left_vel / fastest_wheel * max_wheel_vel
+        right_vel = right_vel / fastest_wheel * max_wheel_vel
+        
+    return left_vel, right_vel
